03949b16e587c5a2d8a00911281a88320c56f20a
Laszlo Kortvelyessy <laci.kortvelyessy@gmai.com>
2023-04-28 01:16:28 +0200

    Created a half workin menu

--- Filmtar/main_teszt.cpp
+++ Filmtar/main_teszt.cpp
@@ -3,19 +3,57 @@
 #include "dokumentum.h"
 #include "tar.h"
 
+using namespace std;
+
 int main(){
     Tar tar;
-    Film film;
-    Dokumentum dok;
-    Csaladi csal;
-    film.beolvas();
-    dok.beolvas();
-    csal.beolvas();
-    tar.add(new Film(film));
-    tar.add(new Dokumentum(dok));
-    tar.add(new Csaladi(csal));
-    tar.lista();
-    tar.torol(1);
-    tar.lista();
+    char valasztas;
+    while(valasztas != 'e'){
+        cout << endl << "a: Hozzaadas" << endl << "b: Torles" << endl << "c: Listazas" << endl << "e: EXIT" << endl;
+        cin >> valasztas;
+        cin.ignore();
+        switch(valasztas){
+            case 'a':{
+                char tipus;
+                cout << endl << "a: Film" << endl << "b: Csaladifilm" << endl << "c: Dokumentumfilm" << endl << "e: VISSZA" << endl;
+                cin >> tipus;
+                cin.ignore();
+                switch(tipus){
+                    case 'a':{
+                        Film film;
+                        film.beolvas();
+                        tar.add(new Film(film));
+                    }
+                    break;
+                    case 'b':{
+                        Csaladi csaladi;
+                        csaladi.beolvas();
+                        tar.add(new Csaladi(csaladi));
+                    }
+                    break;
+                    case 'c':{
+                        Dokumentum dokumentum;
+                        dokumentum.beolvas();
+                        tar.add(new Dokumentum(dokumentum));
+                    }
+                    break;
+                    case 'e':
+                        break;
+                    default:
+                        cout << "Ilyen film tipus nincsen!" << endl;
+                }
+            }break;
+            case 'b':{
+
+            }break;
+            case 'c':{
+
+            }break;
+            case 'e':
+                break;
+            default:
+                cout << "Az alabbi menupontok kozul valasszon!" << endl;
+        }
+    }
     return 0;
 }
